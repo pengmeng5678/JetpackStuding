7892ff854ad5a4ab71455738723bff4aab21a1fb
pengmeng56789 <pengmeng5678@163.com>
2019-11-18 07:32:38 +0800

    add myJetpackstudingKt module

---
+++ myjetpackstudingkt/src/main/java/com/studing/myjetpackstudingkt/ui/ScrollingActivity.kt
@@ -0,0 +1,20 @@
+package com.studing.myjetpackstudingkt.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
+import com.studing.myjetpackstudingkt.R
+import kotlinx.android.synthetic.main.activity_scrolling.*
+
+class ScrollingActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_scrolling)
+        setSupportActionBar(toolbar)
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
+        }
+    }
+}
