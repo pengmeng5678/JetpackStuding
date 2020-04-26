10bcbe0fb244f8886ec74d351fdc9ee2a454be41
pengmeng <852025030@qq.com>
2020-04-26 16:58:04 +0800

    1.更新常见问答笔记

--- myjetpackstudingkt/src/main/java/com/studing/myjetpackstudingkt/decode.kt
+++ myjetpackstudingkt/src/main/java/com/studing/myjetpackstudingkt/decode.kt
@@ -9,18 +9,40 @@ package com.studing.myjetpackstudingkt
 
 
 import java.io.File
+import java.io.FileInputStream
+import java.io.FileOutputStream
 import java.io.IOException
 
 object decode {
 
-    @Throws(IOException::class)
+    /*@Throws(IOException::class)
     @JvmStatic
     fun main(args: Array<String>) {
         println("decode is start:$args")
-        val sourceDir = File("E:\\开发资料\\QH项目资料\\知音\\ZhiYin素材\\零售助手")
-        changeName(sourceDir)
-    }
+//        val sourceDir = File("E:\\开发资料\\QH项目资料\\知音\\ZhiYin素材\\零售助手")
+        val sourceDir = File("C:\\Users\\admin\\Desktop\\Test")
+        val desDir = File("C:\\Users\\admin\\Desktop\\Test1")
 
+        copyfile(sourceDir,desDir)
+//        changeName(sourceDir)
+    }*/
+    @JvmStatic
+    fun main(args: Array<String>) {
+        try {
+            val fis = FileInputStream("C:\\Users\\admin\\Desktop\\Test\\aa.java")
+            val fos = FileOutputStream("C:\\Users\\admin\\Desktop\\Test\\bb.ck")
+            val temp = ByteArray(1024)
+            var len = -1
+            while (fis.read(temp).let { len = it; it != -1}) {
+                fos.write(temp, 0, len)
+            }
+            fis.close()
+            fos.close()
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+
+    }
     private fun changeName(sourceDir: File) {
         if(sourceDir==null){
             return
@@ -36,7 +58,57 @@ object decode {
             }
         }
     }
-
+    @Throws(IOException::class)
+    private fun copyfile(inf: File, outf: File) {
+        var outf = outf
+        if (inf.absolutePath.contains(".svn")) {
+            return
+        }
+        println("from " + inf.absolutePath + ", to " + outf.absolutePath)
+        if (inf.isDirectory) {
+            outf.mkdirs()
+            val files = inf.listFiles()
+            for (file in files) {
+                copyfile(file, File(outf.absolutePath + "\\" + file.name))
+            }
+        } else {
+            var isChanged = false
+            if (outf.name.endsWith(".java")) {
+                outf = File(outf.absolutePath.replace(".java", ".py0"))
+                isChanged = true
+            } else if (outf.name.endsWith(".c")) {
+                outf = File(outf.absolutePath.replace(".c", ".py1"))
+                isChanged = true
+            } else if (outf.name.endsWith(".cpp")) {
+                outf = File(outf.absolutePath.replace(".cpp", ".py2"))
+                isChanged = true
+            } else if (outf.name.endsWith(".so")) {
+                outf = File(outf.absolutePath.replace(".so", ".py3"))
+                isChanged = true
+            } else if (outf.name.endsWith(".h")) {
+                outf = File(outf.absolutePath.replace(".h", ".py4"))
+                isChanged = true
+            }else if (outf.name.endsWith(".docx")) {
+                outf = File(outf.absolutePath.replace(".docx", ".py5"))
+                isChanged = true
+            }
+            val `is` = FileInputStream(inf)
+            val os = FileOutputStream(outf)
+            val buf = ByteArray(1024)
+            var len = 0
+//            while ((len = `is`.read(buf)) != -1) {
+            while ( `is`.read(buf).let { len = it;it!=-1 }) {
+                os.write(buf, 0, len)
+                os.flush()
+            }
+            os.close()
+            `is`.close()
+            if (isChanged) {
+//                inf.delete()
+//                println("delete file:" + inf.absolutePath)
+            }
+        }
+    }
     fun deleteDir(dir: File?): Boolean {
         if (dir == null) {
             return false
